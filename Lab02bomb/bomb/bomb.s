d92ab3ec6c9bbc9e3b2faa6e09712e4e595fabb7
Yu <1526217168@qq.com>
2024-04-20 14:23:18 +0800

    phase_5

--- Lab02bomb/bomb/bomb.s
+++ Lab02bomb/bomb/bomb.s
@@ -446,7 +446,7 @@ Disassembly of section .text:
   40100c:	48 83 ec 18          	sub    $0x18,%rsp
   401010:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx # 2nd arg addr in stack
   401015:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx # 1st arg addr in stack
-  40101a:	be cf 25 40 00       	mov    $0x4025cf,%esi
+  40101a:	be cf 25 40 00       	mov    $0x4025cf,%esi # x/s 0x4025cf -> "%d %d"
   40101f:	b8 00 00 00 00       	mov    $0x0,%eax
   401024:	e8 c7 fb ff ff       	call   400bf0 <__isoc99_sscanf@plt>
   401029:	83 f8 02             	cmp    $0x2,%eax # %eax == 0x2 -> 2 args(guess)
@@ -469,40 +469,42 @@ Disassembly of section .text:
 0000000000401062 <phase_5>:
   401062:	53                   	push   %rbx
   401063:	48 83 ec 20          	sub    $0x20,%rsp
-  401067:	48 89 fb             	mov    %rdi,%rbx
-  40106a:	64 48 8b 04 25 28 00 	mov    %fs:0x28,%rax
+  401067:	48 89 fb             	mov    %rdi,%rbx # input "9fe567" -> %rbx == %rdi == 0x6038c0
+  40106a:	64 48 8b 04 25 28 00 	mov    %fs:0x28,%rax # %fs:0x28 Segment:Offset -> addr
   401071:	00 00 
-  401073:	48 89 44 24 18       	mov    %rax,0x18(%rsp)
-  401078:	31 c0                	xor    %eax,%eax
-  40107a:	e8 9c 02 00 00       	call   40131b <string_length>
-  40107f:	83 f8 06             	cmp    $0x6,%eax
+  401073:	48 89 44 24 18       	mov    %rax,0x18(%rsp) # %rax == 0xbc770700
+  401078:	31 c0                	xor    %eax,%eax # %eax -> 0x0
+  40107a:	e8 9c 02 00 00       	call   40131b <string_length> # arg %rdi
+  40107f:	83 f8 06             	cmp    $0x6,%eax # input length should be 0x6
   401082:	74 4e                	je     4010d2 <phase_5+0x70>
   401084:	e8 b1 03 00 00       	call   40143a <explode_bomb>
   401089:	eb 47                	jmp    4010d2 <phase_5+0x70>
-  40108b:	0f b6 0c 03          	movzbl (%rbx,%rax,1),%ecx
-  40108f:	88 0c 24             	mov    %cl,(%rsp)
-  401092:	48 8b 14 24          	mov    (%rsp),%rdx
-  401096:	83 e2 0f             	and    $0xf,%edx
-  401099:	0f b6 92 b0 24 40 00 	movzbl 0x4024b0(%rdx),%edx
-  4010a0:	88 54 04 10          	mov    %dl,0x10(%rsp,%rax,1)
-  4010a4:	48 83 c0 01          	add    $0x1,%rax
-  4010a8:	48 83 f8 06          	cmp    $0x6,%rax
+  # --------------------------------LOOP---------------------------------
+  40108b:	0f b6 0c 03          	movzbl (%rbx,%rax,1),%ecx # loop entry,%ecx = M[%rbx+%rax],%rax == 0 at first
+  40108f:	88 0c 24             	mov    %cl,(%rsp) # %cl -> lowest 4 bytes in %rcx , put each char of input into M[%rsp]
+  401092:	48 8b 14 24          	mov    (%rsp),%rdx # %rdx == input char
+  401096:	83 e2 0f             	and    $0xf,%edx # save lowest 4 bytes in %edx
+  401099:	0f b6 92 b0 24 40 00 	movzbl 0x4024b0(%rdx),%edx # %edx = M[0x4024b0 + %rdx],0x4024b0->sth about secret stage
+  4010a0:	88 54 04 10          	mov    %dl,0x10(%rsp,%rax,1) # %dl -> lowest 4 bytes in %rdx
+  4010a4:	48 83 c0 01          	add    $0x1,%rax 
+  4010a8:	48 83 f8 06          	cmp    $0x6,%rax # %rax -> i, test if ++i != 6 ? jne(loop) : movb
   4010ac:	75 dd                	jne    40108b <phase_5+0x29>
-  4010ae:	c6 44 24 16 00       	movb   $0x0,0x16(%rsp)
-  4010b3:	be 5e 24 40 00       	mov    $0x40245e,%esi
-  4010b8:	48 8d 7c 24 10       	lea    0x10(%rsp),%rdi
-  4010bd:	e8 76 02 00 00       	call   401338 <strings_not_equal>
+  # --------------------------------LOOP---------------------------------
+  4010ae:	c6 44 24 16 00       	movb   $0x0,0x16(%rsp) # '\0' ,end of string
+  4010b3:	be 5e 24 40 00       	mov    $0x40245e,%esi # x/s 0x40245e -> "flyers",1st arg in <string_not_equal> 
+  4010b8:	48 8d 7c 24 10       	lea    0x10(%rsp),%rdi # 2nd arg in <string_not_equal>
+  4010bd:	e8 76 02 00 00       	call   401338 <strings_not_equal> # if equal -> %rax == 0
   4010c2:	85 c0                	test   %eax,%eax
   4010c4:	74 13                	je     4010d9 <phase_5+0x77>
   4010c6:	e8 6f 03 00 00       	call   40143a <explode_bomb>
   4010cb:	0f 1f 44 00 00       	nopl   0x0(%rax,%rax,1)
   4010d0:	eb 07                	jmp    4010d9 <phase_5+0x77>
-  4010d2:	b8 00 00 00 00       	mov    $0x0,%eax
+  4010d2:	b8 00 00 00 00       	mov    $0x0,%eax # 1st jmp point after get input
   4010d7:	eb b2                	jmp    40108b <phase_5+0x29>
   4010d9:	48 8b 44 24 18       	mov    0x18(%rsp),%rax
-  4010de:	64 48 33 04 25 28 00 	xor    %fs:0x28,%rax
+  4010de:	64 48 33 04 25 28 00 	xor    %fs:0x28,%rax # compare M[%rsp + 0x18] with M[%fs + 0x28]
   4010e5:	00 00 
-  4010e7:	74 05                	je     4010ee <phase_5+0x8c>
+  4010e7:	74 05                	je     4010ee <phase_5+0x8c> # if equal, success defuse
   4010e9:	e8 42 fa ff ff       	call   400b30 <__stack_chk_fail@plt>
   4010ee:	48 83 c4 20          	add    $0x20,%rsp
   4010f2:	5b                   	pop    %rbx
@@ -689,10 +691,10 @@ Disassembly of section .text:
   40131b:	80 3f 00             	cmpb   $0x0,(%rdi)
   40131e:	74 12                	je     401332 <string_length+0x17>
   401320:	48 89 fa             	mov    %rdi,%rdx
-  401323:	48 83 c2 01          	add    $0x1,%rdx
+  401323:	48 83 c2 01          	add    $0x1,%rdx # point to next char
   401327:	89 d0                	mov    %edx,%eax
-  401329:	29 f8                	sub    %edi,%eax
-  40132b:	80 3a 00             	cmpb   $0x0,(%rdx)
+  401329:	29 f8                	sub    %edi,%eax # compute distance (string length)
+  40132b:	80 3a 00             	cmpb   $0x0,(%rdx) # check if the next char is '\0'
   40132e:	75 f3                	jne    401323 <string_length+0x8>
   401330:	f3 c3                	repz ret 
   401332:	b8 00 00 00 00       	mov    $0x0,%eax
@@ -704,12 +706,12 @@ Disassembly of section .text:
   40133b:	53                   	push   %rbx
   40133c:	48 89 fb             	mov    %rdi,%rbx
   40133f:	48 89 f5             	mov    %rsi,%rbp
-  401342:	e8 d4 ff ff ff       	call   40131b <string_length>
-  401347:	41 89 c4             	mov    %eax,%r12d
-  40134a:	48 89 ef             	mov    %rbp,%rdi
-  40134d:	e8 c9 ff ff ff       	call   40131b <string_length>
+  401342:	e8 d4 ff ff ff       	call   40131b <string_length> # get 1st arg length(%rdi)
+  401347:	41 89 c4             	mov    %eax,%r12d # result stored in %r12d
+  40134a:	48 89 ef             	mov    %rbp,%rdi # %rsi -> %rbp -> %rdi
+  40134d:	e8 c9 ff ff ff       	call   40131b <string_length> # get 2nd arg length(%rsi),stored in %rax
   401352:	ba 01 00 00 00       	mov    $0x1,%edx
-  401357:	41 39 c4             	cmp    %eax,%r12d
+  401357:	41 39 c4             	cmp    %eax,%r12d # 2nd arg len, 1st arg len
   40135a:	75 3f                	jne    40139b <strings_not_equal+0x63>
   40135c:	0f b6 03             	movzbl (%rbx),%eax
   40135f:	84 c0                	test   %al,%al
@@ -732,7 +734,7 @@ Disassembly of section .text:
   40138f:	ba 01 00 00 00       	mov    $0x1,%edx
   401394:	eb 05                	jmp    40139b <strings_not_equal+0x63>
   401396:	ba 01 00 00 00       	mov    $0x1,%edx
-  40139b:	89 d0                	mov    %edx,%eax
+  40139b:	89 d0                	mov    %edx,%eax # not equal, %eax == 0x1
   40139d:	5b                   	pop    %rbx
   40139e:	5d                   	pop    %rbp
   40139f:	41 5c                	pop    %r12
